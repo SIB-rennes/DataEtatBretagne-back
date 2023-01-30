78cadccbd664252a911094091687195ffddfad24
JOLIVET Sylvain <sylvain.jolivet.presta@sib.fr>
2023-01-30 10:12:19 +0100

    Remove flask script

--- manage.py
+++ manage.py
@@ -6,12 +6,9 @@ Use "python manage.py runserver --help" for a list of runserver options.
 """
 
 from app import create_app_base
-from flask_script import Manager
 
 
 app_flask = create_app_base(init_falsk_migrate=False)
-manager = Manager(app_flask)
 
 if __name__ == "__main__":
-
-    manager.run()
+    app_flask.run()
