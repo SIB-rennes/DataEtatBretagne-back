4187cb0b4135919764c48a6d4eee0d0b59e6c3fe
gautierr <romain.gautier@sib.fr>
2023-06-22 11:07:17 +0200

    [FEAT] Adapter pour rabbitMQ

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -1,10 +1,10 @@
 import logging
+import pika
+import time
 from functools import wraps
-from time import sleep
 
 from flask import current_app
 from app import celeryapp
-import redis
 
 logger = logging.getLogger()
 celery = celeryapp.celery
@@ -18,33 +18,64 @@ TIMEOUT_QUEUE_RETRY = 'timeout_queue_retry'
 DEFAULT_MAX_QUEUE_SIZE = 100000
 DEFAULT_TIMEOUT_QUEUE_RETRY = 60
 
-if current_app.conf.broker_url is None:
-    redis_conn = None
-else :
-    redis_conn = redis.from_url(current_app.conf.broker_url)
-
 max_queue_size = current_app.conf[MAX_QUEUE_SIZE] if MAX_QUEUE_SIZE in current_app.conf else DEFAULT_MAX_QUEUE_SIZE
 timeout_queue_retry = current_app.conf[TIMEOUT_QUEUE_RETRY] if TIMEOUT_QUEUE_RETRY in current_app.conf else DEFAULT_TIMEOUT_QUEUE_RETRY
 
+def lazy_rabbit_connector():
+
+    conn = None
+    chan = None
+
+    def get_connection():
+        nonlocal conn
+        nonlocal chan
+
+        if current_app.conf.broker_url is None:
+            return None
+        
+        if conn is None or conn.is_closed: 
+            conn = pika.BlockingConnection(pika.URLParameters(current_app.conf.broker_url))
+        
+        if chan is None or chan.is_closed:
+            chan = conn.channel()
+        
+        return chan
+    
+    return get_connection
+
+lazy_channel = lazy_rabbit_connector()
+
 def limiter_queue(queue_name:str, max_queue_size: int = max_queue_size, timeout_queue_retry: int = timeout_queue_retry):
     """
-    Vérifie que le nombre de message dans la file "queue_name" de redis n'atteint pas la taille max
-    Si taille max dépassé, attente de 1 seconde.
+    Vérifie que le nombre de message dans la file "queue_name" de rabbitmq n'atteint pas la taille max
+    Si taille max dépassé, attente de 10 seconde.
     :return:
     """
     def wrapper(func):
+
+
         @wraps(func)
         def inner_wrapper(*args, **kwargs):
-            if not current_app.conf.task_always_eager and redis_conn is not None:
-                num_retries=0
-                while redis_conn.llen(queue_name) > max_queue_size:
+
+            if not current_app.conf.task_always_eager:
+                num_retries = 0
+
+                while True:
+                    channel = lazy_channel()
+                    queue_info = channel.queue_declare(queue_name, passive = True) if channel is not None else None
+                    msg_count = queue_info.method.message_count
+
+                    if msg_count <= max_queue_size:
+                        break
                     if num_retries >= timeout_queue_retry:
                         raise LimitQueueException(f"Timeout exceeded while waiting for the queue '{queue_name}' to be available.")
-                    logger.warning("Limite de la file redis atteinte. On attend 1 seconde")
-                    sleep(1)
-                    num_retries +=1
 
+                    logger.warning(f"Limite de la file rabbitmq atteinte. On attend 10 secondes")
+                    time.sleep(10)
+                    num_retries += 1
+            
             return func(*args, **kwargs)
+
         return inner_wrapper
     return wrapper
 
